42f051a20261202ae02b751dc1a85c5838a5d62b
nielassis <nielvitorba@gmail.com>
2025-01-22 16:43:38 -0300

    feat: Create GameListDTO for transferring game list data

---
+++ src/main/java/com/project/dslist/dto/GameListDTO.java
@@ -0,0 +1,24 @@
+package com.project.dslist.dto;
+
+import com.project.dslist.entities.GameList;
+
+public class GameListDTO {
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+      id = entity.getId();
+      name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
