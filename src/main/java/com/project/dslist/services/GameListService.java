657e73e4d722a7a35558d814e577d53eb848a088
nielassis <nielvitorba@gmail.com>
2025-01-22 16:43:57 -0300

    feat: Implement GameListService with findAll method

---
+++ src/main/java/com/project/dslist/services/GameListService.java
@@ -0,0 +1,23 @@
+package com.project.dslist.services;
+
+import com.project.dslist.dto.GameListDTO;
+import com.project.dslist.entities.GameList;
+import com.project.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
