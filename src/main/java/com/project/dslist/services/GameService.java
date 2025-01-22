e7cb70399e9c6817745d436a9ab86fa32a1133b6
nielassis <nielvitorba@gmail.com>
2025-01-22 15:38:12 -0300

    feat: Add findById method in GameService to fetch game details by ID

--- src/main/java/com/project/dslist/services/GameService.java
+++ src/main/java/com/project/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.project.dslist.services;
 
+import com.project.dslist.dto.GameDTO;
 import com.project.dslist.dto.GameMinDTO;
 import com.project.dslist.entities.Games;
 import com.project.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Games result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Games> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
