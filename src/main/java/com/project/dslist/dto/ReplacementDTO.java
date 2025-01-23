224b8f8a73517a413941b4e876b2f53fc3648143
nielpd <nielvitorba@gmail.com>
2025-01-23 17:55:37 -0300

    feat: add game list reordering functionality
    - Added method `updateBelongingPosition` in `GameListRepository` to update game position within a list.
    - Updated `GameListService` to include `move` method for handling game reordering logic.
    - Created `ReplacementDTO` to manage reordering request data.
    - Modified `GameListController` to handle game reordering via a new endpoint `POST /lists/{listId}/replacement`.

---
+++ src/main/java/com/project/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,22 @@
+package com.project.dslist.dto;
+
+public class ReplacementDTO {
+    private Integer sourceIndex;
+    private Integer targetIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getTargetIndex() {
+        return targetIndex;
+    }
+
+    public void setTargetIndex(Integer targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+}
--- src/main/java/com/project/dslist/services/GameListService.java
+++ src/main/java/com/project/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.project.dslist.services;
 
 import com.project.dslist.dto.GameListDTO;
 import com.project.dslist.entities.GameList;
+import com.project.dslist.projections.GameMinProjection;
 import com.project.dslist.repositories.GameListRepository;
+import com.project.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int targetIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(targetIndex, obj);
+
+        int min = sourceIndex < targetIndex ? sourceIndex : targetIndex;
+        int max = sourceIndex > targetIndex ? targetIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
